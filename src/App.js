7ba57c95c0843a3055a0ee1c31e7d29c73b2640e
pabin-limbu <changsu.pabin@gmail.com>
2023-01-06 17:46:28 +0800

    creating star match game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+// Math science
+const utils = {
+  // Sum an array
+  sum: (arr) => arr.reduce((acc, curr) => acc + curr, 0),
+
+  // create an array of numbers between min and max (edges included)
+  range: (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i),
+
+  // pick a random number between min and max (edges included)
+  random: (min, max) => min + Math.floor(Math.random() * (max - min + 1)),
+
+  // Given an array of numbers and a max...
+  // Pick a random sum (< max) from the set of all available sums in arr
+  randomSumIn: (arr, max) => {
+    const sets = [[]];
+    const sums = [];
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0, len = sets.length; j < len; j++) {
+        const candidateSet = sets[j].concat(arr[i]);
+        const candidateSum = utils.sum(candidateSet);
+        if (candidateSum <= max) {
+          sets.push(candidateSet);
+          sums.push(candidateSum);
+        }
+      }
+    }
+    return sums[utils.random(0, sums.length - 1)];
+  },
+};
+
+const colors = {
+  used: "lightgreen",
+  wrong: "lightcoral",
+  selected: "deepskyblue",
+  candidate: "deepskyblue",
+};
+
+const PlayNumber = (props) => (
+  <button className="number">{props.number}</button>
+);
 
 function App() {
+  const [stars, setStars] = useState(utils.random(1, 9));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game">
+      <div className="help">
+        Pick 1 or more numbers that sum to the number of stars
+      </div>
+      <div className="body">
+        <div className="stars">
+          {utils.range(1, stars).map((number) => (
+            <div key={stars} className="star" />
+          ))}
+        </div>
+        <div className="play-numbers">
+          {utils.range(1, 10).map((number) => (
+            <PlayNumber key={number} number={number} />
+          ))}
+        </div>
+      </div>
+      <div className="timer">Time remaining : 0s</div>
     </div>
   );
 }
